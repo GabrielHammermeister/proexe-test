7f0958acd17cc1226107799b66b6f08dabf9e2d7
Gabriel Hammermeister <gabriel.hammer523@gmail.com>
2022-01-30 08:28:33 -0300

    initial setup: installing dependencies and setting up the router

---
+++ src/pages/EditPage/EditPage.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export const EditPage = () => {
+    return (
+        <>
+            
+            <h1>Edit Page!</h1>
+
+            <Link to='/home' >Home page</Link>
+        </>
+    )
+}
